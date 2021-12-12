3305bd74d467c0fb93cc6665a5544b58187a7cc1
Frezeh <ezehfrank87@gmail.com>
2021-12-12 14:02:46 +0100

    action-creators

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,41 +1,47 @@
-import * as ActionTypes from './ActionTypes';
-import { baseUrl } from './baseUrl';
+import * as ActionTypes from "./ActionTypes";
+import { baseUrl } from "./baseUrl";
 
 export const addUrl = (url) => ({
   type: ActionTypes.ADD_URL,
-  payload: url
+  payload: url,
 });
 
 export const postUrl = (url) => (dispatch) => {
-
   const newUrl = {
     longUrl: url,
   };
 
-  return fetch(baseUrl + 'url', {
+  return fetch(baseUrl + "url", {
     method: "POST",
     body: JSON.stringify(newUrl),
     headers: {
       "Content-Type": "application/json",
     },
   })
-    .then(response => {
-      if (response.ok) {
-        return response;
-      } else {
-        var error = new Error('Error ' + response.status + ': ' + response.statusText);
-        error.response = response;
+    .then(
+      (response) => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error(
+            "Error " + response.status + ": " + response.statusText
+          );
+          error.response = response;
+          throw error;
+        }
+      },
+      (error) => {
         throw error;
       }
-    },
-      error => {
-        throw error;
-      })
-    .then(response => response.json())
-    .then(url => { 
-        localStorage.setItem('code', url.urlCode);
-      dispatch(addUrl(url)); })
-    .catch(error => { alert('Your URL could not be shortened\nError: ' + error.message); });
+    )
+    .then((response) => response.json())
+    .then((url) => {
+      localStorage.setItem("code", url.urlCode);
+      dispatch(addUrl(url));
+    })
+    .catch((error) => {
+      alert("Your URL could not be shortened\nError: " + error.message);
+    });
 };
 
 export const fetchStats = (id) => (dispatch) => {
@@ -43,37 +49,41 @@ export const fetchStats = (id) => (dispatch) => {
   dispatch(historyLoading());
 
   return fetch(baseUrl + `history/${id}`)
-    .then(response => {
-      if (response.ok) {
-        return response;
-      } else {
-        var error = new Error('Error ' + response.status + ': ' + response.statusText);
-        error.response = response;
-        throw error;
-      }
-    },
-      error => {
+    .then(
+      (response) => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error(
+            "Error " + response.status + ": " + response.statusText
+          );
+          error.response = response;
+          throw error;
+        }
+      },
+      (error) => {
         var errmess = new Error(error.message);
         throw errmess;
-      })
-    .then(response => response.json())
-    .then(history => { 
-      localStorage.setItem('stats', history.id)
-      dispatch(addHistory(history))
+      }
+    )
+    .then((response) => response.json())
+    .then((history) => {
+      localStorage.setItem("stats", history.id);
+      dispatch(addHistory(history));
     })
-    .catch(error => dispatch(historyFailed(error.message)));
+    .catch((error) => dispatch(historyFailed(error.message)));
 };
 
 export const historyLoading = () => ({
-  type: ActionTypes.HISTORY_LOADING
+  type: ActionTypes.HISTORY_LOADING,
 });
 
 export const historyFailed = (errmess) => ({
   type: ActionTypes.HISTORY_FAILED,
-  payload: errmess
+  payload: errmess,
 });
 
 export const addHistory = (history) => ({
   type: ActionTypes.ADD_HISTORY,
-  payload: history
+  payload: history,
 });
