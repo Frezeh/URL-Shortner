1039af107a0b451985b76f23dfbf01bf5610717a
Frezeh <ezehfrank87@gmail.com>
2021-12-12 14:01:58 +0100

    statistics

--- src/components/statistics.js
+++ src/components/statistics.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import Link from '@material-ui/core/Link';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Title from './Title';
+import React from "react";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Title from "./Title";
 import { useSelector } from "react-redux";
 
 function preventDefault(event) {
@@ -37,7 +37,7 @@ export default function Statistics() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {history.history.map((hist) => ( 
+          {history.history.map((hist) => (
             <TableRow key={hist?.id}>
               <TableCell>{hist?.date}</TableCell>
               <TableCell>{hist?.time}</TableCell>
